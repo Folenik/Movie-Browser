20c76b57d5ae91eb26fbcca63a13debe95090e39
Folenik <kamil.mosz12@gmail.com>
2020-02-19 12:07:32 +0100

    Data binding setup

--- app/src/main/java/com/kamilmosz/displaymoviesapp/MainActivity.java
+++ app/src/main/java/com/kamilmosz/displaymoviesapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.kamilmosz.displaymoviesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+
+    private void init() {
+        recyclerView.setHasFixedSize(true);
     }
 }
